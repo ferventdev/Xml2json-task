14680e36409041ccb80ef7f1eccea3b34046706f
Aleksandr Shevkunenko <aleksandr_shevkunenko@epam.com>
2019-08-20 19:26:49 +0300

    task basically done with Jackson only

--- src/main/java/com/xml2json/apps/WithJacksonOnly.java
+++ src/main/java/com/xml2json/apps/WithJacksonOnly.java
@@ -4,9 +4,16 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.ObjectWriter;
 import com.fasterxml.jackson.dataformat.xml.XmlMapper;
 import com.xml2json.entities.Catalog;
+import com.xml2json.entities.Model;
 import lombok.SneakyThrows;
 
 import java.net.URI;
+import java.util.LinkedHashMap;
+import java.util.LinkedHashSet;
+import java.util.Map;
+import java.util.Set;
+
+import static java.util.stream.Collectors.*;
 
 public class WithJacksonOnly {
 
@@ -17,7 +24,13 @@ public class WithJacksonOnly {
     @SneakyThrows
     public static void main(String[] args) {
         Catalog catalog = new XmlMapper().readValue(new URI(XML_URL).toURL(), Catalog.class);
-        String json = PRETTY_PRINTER.writeValueAsString(catalog);
+        String json = PRETTY_PRINTER.writeValueAsString(getGroupedModels(catalog));
         System.out.println(json);
+    }
+
+    private static Map<String, Set<String>> getGroupedModels(Catalog catalog) {
+        return catalog.getModels().stream()
+                      .collect(groupingBy(Model::getBrand,
+                              LinkedHashMap::new, mapping(Model::getName, toCollection(LinkedHashSet::new))));
     }
 }
