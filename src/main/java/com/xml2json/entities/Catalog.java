823ece295fd4b08ff8e6803e10fbd2c0d7655af7
Aleksandr Shevkunenko <aleksandr_shevkunenko@epam.com>
2019-08-20 21:01:10 +0300

    useless comments and annotations removed

--- src/main/java/com/xml2json/entities/Catalog.java
+++ src/main/java/com/xml2json/entities/Catalog.java
@@ -3,18 +3,12 @@ package com.xml2json.entities;
 import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlElementWrapper;
 import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlProperty;
 import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlRootElement;
-import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @Getter
-@Setter
-@NoArgsConstructor
-@AllArgsConstructor
 @JacksonXmlRootElement(localName = "Models")
 public class Catalog {
 
--- src/main/java/com/xml2json/entities/Model.java
+++ src/main/java/com/xml2json/entities/Model.java
@@ -1,26 +1,17 @@
 package com.xml2json.entities;
 
 import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlProperty;
-import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
 
 @Getter
-@Setter
-@NoArgsConstructor
-@AllArgsConstructor
 public class Model {
 
-//    @JsonProperty("Category")
     @JacksonXmlProperty(localName = "Category", isAttribute = true)
     private String category;
 
-//    @JsonProperty("Make")
     @JacksonXmlProperty(localName = "Make", isAttribute = true)
     private String brand;
 
-//    @JsonProperty("Model")
     @JacksonXmlProperty(localName = "Model", isAttribute = true)
     private String name;
 }
